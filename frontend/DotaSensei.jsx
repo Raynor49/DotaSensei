ed8e71141f8e04d29136cc92903661958ee541f1
Willis Roberts <willisroberts49@gmail.com>
2018-04-18 17:48:05 -0400

    frontend main page

---
+++ frontend/DotaSensei.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>DotaSensei h1 here</h1>, root);
+})
