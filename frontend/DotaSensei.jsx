27b8a7cefd14ea8b5d226e6902f0e605ad5674c5
Willis Roberts <willisroberts49@gmail.com>
2018-04-29 16:25:57 -0400

    App and Root files in frontend

--- frontend/DotaSensei.jsx
+++ frontend/DotaSensei.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+import Root from './component/root.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore({})
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>DotaSensei h1 here</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 })
