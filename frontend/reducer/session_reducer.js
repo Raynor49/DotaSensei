fa33ff937724e3f91a6268050be36670d16618ee
Willis Roberts <willisroberts49@gmail.com>
2018-05-06 14:18:41 -0400

    session_reducer

---
+++ frontend/reducer/session_reducer.js
@@ -0,0 +1,16 @@
+import merge from 'lodash/merge'
+import { RECEIVE_CURRENT_USER } from '../actions/sessino_actions.js';
+
+const _defaultState = { currentUser: null }
+
+const sessionReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, { currentUser: action.currentUser });
+    default:
+      return state;
+  }
+}
+
+export default sessionReducer;
