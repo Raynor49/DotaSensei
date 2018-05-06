48030ab48ea62206dec34aa022c8145548a8f507
Willis Roberts <willisroberts49@gmail.com>
2018-05-06 14:08:54 -0400

    session_actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,37 @@
+import * as APIUtil from '../util/session_api_util.js'
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+
+export const clearErrors = () => {
+  return({
+    type: CLEAR_ERRORS
+  });
+};
+
+export const receiveCurrentUser = (currentUser) => {
+  return({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  })
+}
+
+
+export const login = (user) => dispatch => {
+  return APIUtil.login(user).then(
+    user => (dispatch(receiveCurrentUser(user)))
+  )
+}
+
+export const signup = (user) => dispatch => {
+  return APIUtil.signup(user).then(
+    user => (dispatch(receiveCurrentUser(user)))
+  )
+}
+
+export const logout = () => dispatch => {
+  return APIUtil.logout().then(
+    () => (dispatch(receiveCurrentUser(null)))
+  )
+}
