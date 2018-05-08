2c961b3e7df0a95c66aa2b5029407a9d434359e5
Willis Roberts <willisroberts49@gmail.com>
2018-05-08 13:47:41 -0400

    session_form.jsx

---
+++ frontend/component/session/session_form.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+
+class SessionForm extends React.Component{
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  render() {
+    return(
+      <div>This is the session page</div>
+    )
+  }
+}
+
+export default withRouter(SesssionForm);
