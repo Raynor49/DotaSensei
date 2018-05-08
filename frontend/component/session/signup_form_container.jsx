ba3d4850eb6a7039ba43d3aae5a4e4c7593936c8
Willis Roberts <willisroberts49@gmail.com>
2018-05-08 13:41:25 -0400

    session component

---
+++ frontend/component/session/signup_form_container.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+
+import React form 'react';
+
+const mapStateToProps = (state) => {
+  return({
+    formType: Signup
+  })
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return({
+
+  })
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)()
